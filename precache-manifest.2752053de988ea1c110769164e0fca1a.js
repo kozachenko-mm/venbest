4f85839c117276a43b7cd87219305f0539e06b30
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2020-10-18 19:03:01 +0300

    Updates

---
+++ precache-manifest.2752053de988ea1c110769164e0fca1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fb28e9e2c9cacfafb03a1b2601420b",
+    "url": "/venbest/index.html"
+  },
+  {
+    "revision": "4ac3faa1c703302e14a3",
+    "url": "/venbest/static/css/main.252e8517.chunk.css"
+  },
+  {
+    "revision": "5c2ad2df395643c49d69",
+    "url": "/venbest/static/js/2.4d88a073.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/venbest/static/js/2.4d88a073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac3faa1c703302e14a3",
+    "url": "/venbest/static/js/main.c93cca4e.chunk.js"
+  },
+  {
+    "revision": "f81f2e6c035a2c893a03",
+    "url": "/venbest/static/js/runtime-main.5ac2361c.js"
+  }
+]);
